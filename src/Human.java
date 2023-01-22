f96c8bd8fe085ed52b5a8d7b29b14c940737d174
linirs <linirs12345@gmail.com>
2023-01-22 16:33:02 +0100

    Zad 3 i 4 - Get your salary & give yourself a rise

--- src/Human.java
+++ src/Human.java
@@ -1,13 +1,43 @@
+import java.time.LocalDateTime;
+
+
 public class Human {
     String firstName;
     String lastName;
     Integer yearOfBirth;
     Animal pet;
     Car car;
+    private Double salary;
+    private Double prevSalary;
+    private LocalDateTime salaryGetDate;
     public Human(String firstName, String lastName,Integer yearOfBirth){
         this.firstName = firstName;
         this.lastName = lastName;
         this.yearOfBirth = yearOfBirth;
+    }
+    public void setSalary(double newSalary){
+        if (newSalary < 0 ){
+            System.out.println("Can't set negative salary");
+            return;
+        }
+        System.out.println("Nowe dane zostały wysłane do systemu księgowego");
+        System.out.println("Prosimy o odebranie aneksu do umowy od pani Hani z kadr");
+        System.out.println("ZUS i US już wiedzą o zmianie wypłaty i nie ma sensu ukrywać dochodu");
+        this.salary = newSalary;
+    }
+
+    public double getSalary(){
+        if(salaryGetDate != null){
+            System.out.println("Poprzednia data pobrania wyplaty to: " + this.salaryGetDate);
+            System.out.println("Poprzednia wartość wypłaty to: " + this.prevSalary);
+            this.salaryGetDate = LocalDateTime.now();
+            this.prevSalary = this.salary;
+            return this.salary;
+        }
+        this.salaryGetDate = LocalDateTime.now();
+        this.prevSalary = this.salary;
+        System.out.println("Brak daty poprzedniego pobrania wyplaty");
+        return this.salary;
     }
     public void setPet(Animal pet){
         this.pet = pet;
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Human czlek = new Human("Rysiek", "Wisnia", 1997);
-        Car fiat = new Car("Punto", "Fiat");
-        czlek.setCar(fiat);
 
+        czlek.setSalary(2000);
+        System.out.println();
+        czlek.getSalary();
+        System.out.println();
+        czlek.setSalary(2200);
+        System.out.println();
+        czlek.getSalary();
+        System.out.println();
+        czlek.getSalary();
     }
 }
